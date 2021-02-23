819ade2e43be342ca9857d871658a6a094e1a49f
zanderthuo <thuozander@gmail.com>
2021-02-23 17:20:03 +0300

    created homepage and styled it

---
+++ src/components/Product.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import '../styles/Product.css'
+
+function Product({ title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>KES. </small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button className="product__button">Add To Cart</button>
+    </div>
+  )
+}
+
+export default Product
