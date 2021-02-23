0218bb8340c33896d3cde5b561c972b353ab8698
zanderthuo <thuozander@gmail.com>
2021-02-23 10:41:17 +0300

    added header component and styled it

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="app">
+      {/*Header*/}
+      <Header />
+      {/*Home*/}
+      {/**/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSHCg5WLRqZa8LmPtF1RiBuKdfsn6xnV0",
+  authDomain: "clone-1c657.firebaseapp.com",
+  projectId: "clone-1c657",
+  storageBucket: "clone-1c657.appspot.com",
+  messagingSenderId: "423196451349",
+  appId: "1:423196451349:web:b218af372e01cf64f8641c"
+};
